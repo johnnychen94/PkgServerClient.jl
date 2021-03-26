5248053663fb4bd83f5b0f10f32e28198bcea5c1
Documenter.jl <documenter@juliadocs.github.io>
2021-03-26 07:18:17 +0000

    build based on 7b8a412

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PkgServerClient","category":"page"},{"location":"#PkgServerClient","page":"Home","title":"PkgServerClient","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PkgServerClient.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PkgServerClient]","category":"page"}]
+}
